76b86ea48e08923c4d20f8b25be592c7e5fe1401
Miklas Karjalainen <miklas.karjalainen@gmail.com>
2022-12-09 11:35:22 +0200

    timing

--- src/giffiscript.rs
+++ src/giffiscript.rs
@@ -14,12 +14,32 @@ impl GiffiScript {
     }
 
     pub fn execute(&mut self, code: String) {
-        let ltokens = Lexer::lex(code);
-        println!("Lexer: {:?}", ltokens);
+        use colored::Colorize;
+        use std::time::Instant;
 
-        let ptokens = Parser::parse(ltokens);
-        println!("Parser: {:?}", ptokens);
+        println!("{}", format!("<---Lexing Started!--->").green().bold());
+            let now = Instant::now();
+            let ltokens = Lexer::lex(code);
+            let end = Instant::now();
+            let lexer_time = end - now;
+            println!("Lexer Result: {:#?}", ltokens);
+        println!("{}", format!(">---Lexing Ended!---<").green().bold());
 
+        println!("{}", format!("<---Parsing Started!--->").cyan().bold());
+            let now = Instant::now();
+            let ptokens = Parser::parse(ltokens);
+            let end = Instant::now();
+            let parser_time = end - now;
+        println!("Parser Result: {:#?}", ptokens);
+        println!("{}", format!(">---Parsing Ended!---<").cyan().bold());
+        
+        let now = Instant::now();
         self.interpreter.execute_tokens(&ptokens);
+        let end = Instant::now();
+        let interpreting_time = end - now;
+
+        println!("Lexing Time: {:?}", lexer_time);
+        println!("Parsing Time: {:?}", parser_time);
+        println!("Interpriting Time: {:?}", interpreting_time);
     }
 }
